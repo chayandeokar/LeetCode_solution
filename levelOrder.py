3c63959159e9a793f29a4ad89a1f2d0f41251506
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-02-02 00:01:59 +0530

    Create levelOrder.py

---
+++ levelOrder.py
@@ -0,0 +1,20 @@
+# 102. Binary Tree Level Order Traversal
+# https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        result, current = [], [root]
+        while current:
+            next_level, vals = [], []
+            for node in current:
+                vals.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            current = next_level
+            result.append(vals)
+        return result
